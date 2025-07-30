72e98f4c712f29e36fe40076332dfefcde944ef1
Esther Kica <kicaest03@gmail.com>
2025-07-30 10:34:19 +0300

    2nd commit

--- Intro_to_python/calculator.py
+++ Intro_to_python/calculator.py
@@ -1,7 +1,7 @@
 # Simple calculator in Python
 # Asking the user to input two numbers and an operation
-num1 = (input("Enter the first number: "))
-num2 = (input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 
 # Perform the chosen operation
